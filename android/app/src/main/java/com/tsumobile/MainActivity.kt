c4be31a7dc67ac4a8e6e810d3fce5c8d61292589
Бегенч Гельдыев <b.geldyev@dnomads.pro>
2026-02-22 12:11:21 +0700

    feature: init project

--- android/app/src/main/java/com/tsumobile/MainActivity.kt
+++ android/app/src/main/java/com/tsumobile/MainActivity.kt
@@ -11,7 +11,7 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "TsuMobile"
+  override fun getMainComponentName(): String = "PickMe"
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
--- ios/TsuMobile/AppDelegate.swift
+++ ios/TsuMobile/AppDelegate.swift
@@ -24,7 +24,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     window = UIWindow(frame: UIScreen.main.bounds)
 
     factory.startReactNative(
-      withModuleName: "TsuMobile",
+      withModuleName: "PickMe",
       in: window,
       launchOptions: launchOptions
     )
